713dd052986f257f550afcde0f465ceb08b0bc4a
Jochen Wezel <jwezel@compumaster.de>
2017-04-06 14:49:51 +0200

    use NuGet to reference Jayrock.Json library
    incl. update to VS.2017

---
+++ VTigerApi/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VTigerApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Open Source")]
+[assembly: AssemblyProduct("VTigerApi")]
+[assembly: AssemblyCopyright("Copyleft © 2014, 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1c13d0b8-ef2b-4656-b9bc-cc256fa26a5d")]
+
+[assembly: AssemblyVersion("2017.04.06.100")]
+

79cde2e3c80a523644c9a342664365b7a52abbe9
Jochen Wezel <jwezel@compumaster.de>
2020-11-13 16:17:13 +0100

    refreshed dependencies

--- VTigerManagerDemo/Properties/AssemblyInfo.cs
+++ VTigerManagerDemo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CompuMaster")]
 [assembly: AssemblyProduct("VTigerManagerDemo")]
-[assembly: AssemblyCopyright("2014, 2017 CompuMaster GmbH & additional committers")]
+[assembly: AssemblyCopyright("2014, 2017, 2020 CompuMaster GmbH & additional committers")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8bd2caf0-a465-4f61-8f78-d7844d9d16ec")]
 
-[assembly: AssemblyVersion("2017.09.06.100")]
+[assembly: AssemblyVersion("2020.11.13.100")]
 
--- VTigerManagerDemoControls/Properties/AssemblyInfo.cs
+++ VTigerManagerDemoControls/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CompuMaster GmbH")]
 [assembly: AssemblyProduct("VTigerUserControls")]
-[assembly: AssemblyCopyright("2014, 2017, 2019 CompuMaster GmbH & additional committers")]
+[assembly: AssemblyCopyright("2014, 2017, 2019, 2020 CompuMaster GmbH & additional committers")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("58838d58-d523-4851-9d12-c8bcc59deada")]
 
-[assembly: AssemblyVersion("2019.02.21.101")]
+[assembly: AssemblyVersion("2020.11.13.100")]
 
 //[Assembly: InternalsVisibleTo("VTigerApiUnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001006F6DB3A18C235CAA4B7C902340D5C0BCF440328CEB7E695A98E1325C6EB1D517F81A18DA9E355408A269ABA4C6E341C2239CEF880C1D2BA308E104C3BA030368C3F3E19A4CA4B7DE6E015E35D88F16C82EC883D3DE6E598861C95C96A8D0E8A1EE4013DDE41C6B2D1E4A86DCCF868AA9A3C4BAA5BB2E48547AECEB7A4EF0F0CA")]
 //[assembly: InternalsVisibleTo("VTigerApiUnitTests, PublicKey=null")]

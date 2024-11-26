d76ce2f441dbeffbf5bb2608189c4ca195595c28
Jochen Wezel <jwezel@compumaster.de>
2024-11-26 13:14:55 +0100

    added GlobalSuppresions

---
+++ VTigerApi/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: SuppressMessage("Style", "IDE0017:Initialisierung von Objekten vereinfachen", Justification = "<Ausstehend>")]
+[assembly: SuppressMessage("Style", "CS1591:Fehledes XML-Kommentar für öffentlich sichtbaren Typ oder Element", Justification = "<Ausstehend>")]
+[assembly: SuppressMessage("Style", "CA1416:Diese Aufrufsite ist auf allen Plattformen erreichbar. \"Control.Refresh()\" nur unterstützt für: windows, 6.1 und höhere Versionen.", Justification = "<Ausstehend>")]
